54b9bce9b984f011544de7fca947e929248b226e
Lorenzo Pecchio <noreply.83062271+loky-lp@lorenzopecchio.com>
2024-06-22 11:09:40 +0200

    test export default function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from './router';
 
-addEventListener('fetch', (event) => {
-	return event.respondWith(
-		fetchRequestHandler({
-			endpoint: '/trpc',
-			req: event.request,
-			router: appRouter,
-			createContext: () => ({}),
-		}),
-	);
-});
+export default function(first: object, second: object) {
+	console.log(`The function has started`)
+	console.log('first', first.constructor.name)
+	console.log('second', second.constructor.name)
+
+	return new Response('You are living on the edge')
+}
+
+// addEventListener('fetch', (event) => {
+// 	return event.respondWith(
+// 		fetchRequestHandler({
+// 			endpoint: '/trpc',
+// 			req: event.request,
+// 			router: appRouter,
+// 			createContext: () => ({}),
+// 		}),
+// 	);
+// });
